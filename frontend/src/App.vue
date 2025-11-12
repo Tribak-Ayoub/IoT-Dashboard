e9ba3d07a35a41dc6d01a0cc0e3868588ce1f147
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-11-12 18:00:56 +0100

    updaet App.js to use RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,8 @@
 <template>
-  <h1 class="text-3xl font-bold text-green-600">Tailwind is working</h1>
-
+  <!-- <h1 class="text-3xl font-bold text-green-600">Tailwind is working</h1> -->
+  <RouterView />
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
